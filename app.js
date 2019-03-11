199bf21e1c78d82e208c85ffd125020e6d999935
gillbalraj <balrajgill18@gmail.com>
2019-03-11 12:58:03 -0400

    added login route and middleware

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -56,7 +56,19 @@ app.post("/register", function(req, res){
 
 })
 
-
+//login form
+app.get("/login", function(req, res){
+    res.render("login");
+})
+//
+// Login logic 
+// middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login",
+    failureMessage: "Wrong username or password"
+}) ,function(req,res){
+})
 
 // app.listen(process.env.PORT, process.env.IP, function(){
 //     console.log('Listening to port 8080');
